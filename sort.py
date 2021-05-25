576ae7770bde3f7677bad984651e08726484565d
AndrewHanasiro <andrewkhanasiro@gmail.com>
2021-05-25 20:23:57 -0300

    Co-authored-by: danielwknari@gmail.com <danielwknari@gmail.com>

--- sort.py
+++ sort.py
@@ -1,4 +1,4 @@
-#bubblesort
+# bubblesort
 # [3,5,2,7,1,4,6]
 # [3,2,5,1,4,6,7]
 # [2,3,1,4,5,6,7]
@@ -22,25 +22,26 @@
 #     return lista
 
 def ordenar_bubblesort(lista):
-    esta_ordenado= False
+    esta_ordenado = False
     acumulador = 0
     while esta_ordenado == False:
         esta_ordenado = True
-        for posi in range(len(lista)- 1):
+        for posi in range(len(lista) - 1):
             if lista[posi] > lista[posi + 1]:
                 acumulador = lista[posi + 1]
                 lista[posi + 1] = lista[posi]
                 lista[posi] = acumulador
-                esta_ordenado=False
+                esta_ordenado = False
     return lista
 
-lista = [3,5,2,7,1,4,6]
+
+lista = [3, 5, 2, 7, 1, 4, 6]
 lista_ordenada = ordenar_bubblesort(lista)
 print(lista_ordenada)
 
 
-#-----------------------------------------------------
-#insertion sort
+# -----------------------------------------------------
+# insertion sort
 # [3,5,2,7,1,4,6]
 # [3,5,2,7,1,4,6]
 # [2,3,5,7,1,4,6]
@@ -49,50 +50,53 @@ print(lista_ordenada)
 # [1,2,3,4,5,7,6]
 # [1,2,3,4,5,6,7]
 def coloca_numero_na_lista(lista_ordenada: list, numero: int) -> list:
-    lista_temp=[] 
+    lista_temp = []
     posi = 0
-    numero_foi_inserido=False
+    numero_foi_inserido = False
     while posi < len(lista_ordenada):
         if numero_foi_inserido:
             lista_temp.append(lista_ordenada[posi])
         else:
-            if lista_ordenada[posi] < numero :
+            if lista_ordenada[posi] < numero:
                 lista_temp.append(lista_ordenada[posi])
             else:
                 lista_temp.append(numero)
                 lista_temp.append(lista_ordenada[posi])
-                numero_foi_inserido=True
-        posi=posi+1
+                numero_foi_inserido = True
+        posi = posi+1
     if not numero_foi_inserido:
         lista_temp.append(numero)
     return lista_temp
 
+
 def ordenar_insertion_sort(lista):
-    lista_ordenada=[]
+    lista_ordenada = []
     for posi in range(len(lista)):
-        numero=lista[posi]
+        numero = lista[posi]
         lista_ordenada = coloca_numero_na_lista(lista_ordenada, numero)
     return lista_ordenada
 
-lista = [3,5,2,7,1,4,6]
+
+lista = [3, 5, 2, 7, 1, 4, 6]
 lista_ordenada2 = ordenar_insertion_sort(lista)
 print(lista_ordenada2)
 
 
-#-----------------------------------------------------
-#selection sort
+# -----------------------------------------------------
+# selection sort
 
-#1)procurar o menor numero na parte desordenada
-#2)apaga-lo de sua posicao original
-#3)coloca-lo na ultima posicao do ordenados
-#4) volte para o passo 1 ate que a parte dos desordenados nao exista mais
+# 1)procurar o menor numero na parte desordenada
+# 2)apaga-lo de sua posicao original
+# 3)coloca-lo na ultima posicao do ordenados
+# 4) volte para o passo 1 ate que a parte dos desordenados nao exista mais
 
-def copy_list(lista:list) -> list:
-    tamanho=len(lista)
+def copy_list(lista: list) -> list:
+    tamanho = len(lista)
     result = []
     for l in range(tamanho):
         result.append(lista[l])
     return result
+
 
 def ache_menor_numero_da_lista(lista_ordenada, posi):
     menor = 9999999
@@ -103,75 +107,77 @@ def ache_menor_numero_da_lista(lista_ordenada, posi):
             posi_menor = posi1
     return posi_menor
 
-def coloca_numero_na_lista(posi: int, posi_menor:int, lista_ordenada:list):
+
+def coloca_numero_na_lista(posi: int, posi_menor: int, lista_ordenada: list):
     list_temp = copy_list(lista_ordenada)
     list_temp[posi] = lista_ordenada[posi_menor]
     list_temp[posi_menor] = lista_ordenada[posi]
     return list_temp
 
+
 def ordenar_selection_sort(lista):
     for posi in range(len(lista)):
-        posi_menor=ache_menor_numero_da_lista(lista, posi)        
+        posi_menor = ache_menor_numero_da_lista(lista, posi)
         lista = coloca_numero_na_lista(posi, posi_menor, lista)
     return lista
 
-lista = [3,5,2,7,1,4,6]
+
+lista = [3, 5, 2, 7, 1, 4, 6]
 lista_ordenada3 = ordenar_selection_sort(lista)
 print(lista_ordenada3)
 
-#-----------------------------------------------------
-#merge-sort
-#[3,5,2,7,1,4,6,9,10,8,11]
-#[[3,5],[2,7],[1,4],[6,9],[10,8,11]]
-#[[3,5],[2,7],[1,4],[6,9],[8,10,11]]
+# -----------------------------------------------------
+# merge-sort
+# [3,5,2,7,1,4,6,9,10,8,11]
+# [[3,5],[2,7],[1,4],[6,9],[10,8,11]]
+# [[3,5],[2,7],[1,4],[6,9],[8,10,11]]
 # aqui acaba a devisões
-#[[2,3,5,7],[1,4,6,9],[8,10,11]]
-#[[1,2,3,4,5,6,7,9],[8,10,11]]
-#[[1,2,3,4,5,6,7,8,9,10,11]]
+# [[2,3,5,7],[1,4,6,9],[8,10,11]]
+# [[1,2,3,4,5,6,7,9],[8,10,11]]
+# [[1,2,3,4,5,6,7,8,9,10,11]]
 
-#dividir o conjunto maior em duplas ate precise ou nao do ultimo ser um trio
-#organizar dentro dos mini conjuntos
-#juntar  em duplas em um conjunto e organiza-las
-#repetir passo 2 ate que todos os mini conjuntos estejam juntos e organizados
+# dividir o conjunto maior em duplas ate precise ou nao do ultimo ser um trio
+# organizar dentro dos mini conjuntos
+# juntar  em duplas em um conjunto e organiza-las
+# repetir passo 2 ate que todos os mini conjuntos estejam juntos e organizados
 
-def quantidade_faltante():
 
-def verificar_dup_tri():
+def dividir_em_2_ou_3(lista: list) -> list[list]:  # [5, 3, 7, 2, 1, 4, 6]
+    ls_temp = []
+    dupla = []
+    for posi in range(len(lista)):
+        if(posi % 2 == 0):
+            dupla.append(lista[posi])
+        else:
+            dupla.append(lista[posi])
+            ls_temp.append(dupla[:])
+            dupla = []
+    if(len(dupla) == 1):
+        ls_temp[-1].append(dupla[0])
+    return ls_temp
+
+
+def ordernar_listas(listas: list[list]):  # [[5, 3], [7, 2],[ 1, 4, 6]]
+    for posi in range(len(listas)):
+        if(listas[posi][0] > listas[posi][1]):
+            conj_temp = []
+            conj_temp.append(listas[posi][1])
+            conj_temp.append(listas[posi][0])
+            listas[posi] = conj_temp
+            conj_temp = []
+
+
+def juntar_listas(listas: list[list]) -> list:  # [[3, 5], [2, 7],[ 1, 4, 6]]
     pass
-
-def separador_dupla():
-    pass
-
-def separador_trio():
-    pass
-
-def organizador_dupla():
-    pass
-
-def verif_continue():
-    pass    
 
 
 def ordenar_merge_sort(lista):
-    for number in range (len())
-    verificacao = verificar_dup_tri(lista)
-    while verif_continue == True:
-        lista_dupla_inicial = 
-        lista_tripa_inicial = 
-        
-        if verificacao:
-            lista_dupla = separador_dupla()
-        else:
-            lista_tripa = separador_trio()
-
-        verif_continue()
-
-# [3,5,2,7,1,4,6]
-# verificacao
+    lista_2_ou_3 = dividir_em_2_ou_3(lista)
+    ordernar_listas(lista_2_ou_3)
+    lista_ordenada = juntar_listas(lista_2_ou_3)  # [1, 2, 3, 4, 5, 6, 7]
+    return lista_ordenada
 
 
-
-
-lista = [3,5,2,7,1,4,6]
-lista_ordenada4 = ordenar_merge_sort(lista)
-print(lista_ordenada4)
+lista = [5, 3, 7, 2, 1, 4, 6]
+lista_ordenada3 = ordenar_merge_sort(lista)
+print(lista_ordenada3)
