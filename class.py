4b6543b3b3b622e5822893967e3a2532b24bfca2
danielnarimatsu <danielkenzo1997@gmail.com>
2021-08-12 19:42:27 -0300

    daniel

--- class.py
+++ class.py
@@ -1,46 +1,11 @@
-class Human:
-    def walk(self):
-        pass
 
-    def eat(self):
-        pass
-
-
-class Animal:
-    def walk(self):
-        pass
-
-
-class Dog(Animal):
-    def talk(self):
-        return ("Auff")
-
-
-class Cat(Animal):
-    def talk(self):
-        return ("Miau")
-
-
-human = Human()
-human.eat()
-
-pet1 = Dog()
-pet1.walk()
-
-pet2 = Cat()
-pet3 = Cat()
-pet4 = Cat()
-
-pet2.walk()
-
-print(pet1.talk())
-print(pet2.talk())
 
 
 # Class Car, Moto, Bus, Truck, Vehicle
 #===============================================================
 class Vehicle:
-    pass
+    def walk(self):
+        return("andou")
 
 class Car(Vehicle):
     def wheels(self):
@@ -66,6 +31,7 @@ print(v2.wheels())
 
 v3 = Bus()
 print(v3.wheels())
+print(v3.walk())
 
 v4= Truck()
 print(v4.wheels())
