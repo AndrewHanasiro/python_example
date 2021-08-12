7125f44cc48537137ff5224b371ce5c984c1db08
danielnarimatsu <danielkenzo1997@gmail.com>
2021-08-12 16:19:43 -0300

    apliquei o conceito com veiculos

--- class.py
+++ class.py
@@ -38,3 +38,34 @@ print(pet2.talk())
 
 
 # Class Car, Moto, Bus, Truck, Vehicle
+#===============================================================
+class Vehicle:
+    pass
+
+class Car(Vehicle):
+    def wheels(self):
+        return ("4")
+
+class Moto(Vehicle):
+    def wheels(self):
+        return ("2")
+
+class Bus(Vehicle):
+    def wheels(self):
+        return ("8")
+
+class Truck(Vehicle):
+    def wheels(self):
+        return ("6")
+
+v1 = Car()
+print(v1.wheels())
+
+v2 = Moto()
+print(v2.wheels())
+
+v3 = Bus()
+print(v3.wheels())
+
+v4= Truck()
+print(v4.wheels())
